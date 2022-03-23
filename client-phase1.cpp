1034840b7d3cb7751419c5c0c158bac559555217
Kanishka Mittal <81782027+kanishka-mittal@users.noreply.github.com>
2022-03-23 10:31:34 +0530

    Update client-phase1.cpp

--- client-phase1.cpp
+++ client-phase1.cpp
@@ -8,22 +8,15 @@
 #include <arpa/inet.h>
 #include<dirent.h>
 using namespace std;
-
 int main(int argc, char *argv[]){
-    int no,id,port,nbrs;
-    freopen(argv[0],"r",stdin);
-    cin>>no>>id>>port;
+    int sno,id,myport,nbrs;
+    freopen(argv[2],"r",stdin);
+    cin>>sno>>myport>>id;
     cin>>nbrs;
-
     int nos[nbrs],ports[nbrs];
     for(int i=0;i<nbrs;i++){
         cin>>nos[i]>>ports[i];
     }
-
-    // string path = argv[1];
-    // for (const auto & entry : fs::directory_iterator(path))
-    //     cout <<entry.path()<<endl;
-
     DIR *pDIR;
     struct dirent *entry;
     if( pDIR=opendir(argv[1])){
@@ -33,36 +26,86 @@ int main(int argc, char *argv[]){
             }
         closedir(pDIR);
     }
-
-    int sendfd,recfd;
-
-    struct sockaddr_in send_addr,rec_addr,nbr_addr;
-    sendfd = socket(AF_INET, SOCK_STREAM, 0);
+    int recfd,neighfd;
+    struct sockaddr_in rec_addr,neigh_addr;
     recfd = socket(AF_INET, SOCK_STREAM, 0);
-
     rec_addr.sin_family = AF_INET;
-    rec_addr.sin_port = htons(port);
+    rec_addr.sin_port = htons(myport);
     rec_addr.sin_addr.s_addr = INADDR_ANY;
-
     memset(&(rec_addr.sin_zero), '\0', 8);
-
-    if(bind(recfd, (struct sockaddr *)&rec_addr, sizeof(struct sockaddr)) < 0) exit(0); 
-
-    send_addr.sin_family = AF_INET;
-    send_addr.sin_port = 0;
-    send_addr.sin_addr.s_addr = INADDR_ANY;
-    memset(&(send_addr.sin_zero), '\0', 8);
-
-    if(listen(recfd,20)) exit(0);
-
+    int yes=1;
+    if (setsockopt(recfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+    if(bind(recfd, (struct sockaddr *)&rec_addr, sizeof(struct sockaddr)) < 0){ 
+        perror("binf"); 
+        exit(0); 
+    } 
+    if(listen(recfd,20)){ 
+        perror("listen"); 
+        exit(0); 
+    }
+    int sendfd[nbrs];
+    fd_set master,read_fds;
+    int fdmax,nbytes;
+    //change buffer size
+    char buf[256];
+    FD_ZERO(&master);
+    FD_ZERO(&read_fds);
+    FD_SET(recfd, &master);
+    fdmax = recfd;
     for(int i=0;i<nbrs;i++){
+        sendfd[i]=socket(AF_INET, SOCK_STREAM, 0);
         struct sockaddr_in target_addr;
         target_addr.sin_family = AF_INET;
         target_addr.sin_port = htons(ports[i]);
         target_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
         memset(&(target_addr.sin_zero), '\0', 8); 
-        while(connect(sendfd,(struct sockaddr *)&target_addr, sizeof(struct sockaddr)) <= 0){}
-        //get unique ID 
-        cout<<"Connected to "<<nos[i]<<" with unique-ID "<<123<<" on port "<<ports[i]<<endl;
+        while(connect(sendfd[i],(struct sockaddr *)&target_addr, sizeof(struct sockaddr)) < 0){}
+        FD_SET(sendfd[i], &master);
+        fdmax=max(fdmax,sendfd[i]);
     }
+    for(;;){
+        read_fds=master;
+        if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1){
+            perror("select");
+            exit(1);
+        }
+        for(int i=0;i<=fdmax;i++){
+            if (FD_ISSET(i,&read_fds)){
+                if (i==recfd){
+                    int sin_size=sizeof(struct sockaddr_in);
+                    neighfd=accept(recfd,(struct sockaddr *)&neigh_addr, (socklen_t*)&sin_size);
+                    if(neighfd==-1){
+                        perror("accept");
+                    }else{
+                        nbytes=send(neighfd, to_string(id).c_str(), strlen(to_string(id).c_str()), 0);
+                        if(nbytes<= 0){
+                            if (nbytes == 0) {
+                                printf("selectserver: socket %d hung up\n", i);
+                                shutdown(neighfd,2);
+                            } else {
+                                perror("send");
+                            }
+                        }else{
+                            // cout<<nbytes<<endl;
+                        } 
+                    }
+                }else{
+                    // cout<<"here\n";
+                    nbytes=recv(i, buf, sizeof(buf), 0);
+                    if (nbytes == 0) {
+                        printf("selectserver: socket %d hung up\n", i);
+                        shutdown(i,2);
+                    }else if(nbytes<0){
+                        perror("recv");
+                    }else{  
+                        cout<<"Connected with unique-ID "<<buf<<endl;
+                    }
+                }
+            }
+        }
+    }
+
 }
