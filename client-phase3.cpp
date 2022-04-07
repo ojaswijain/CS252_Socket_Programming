cc965d673182f727b62379c021686f3e28dfab74
Kanishka Mittal <81782027+kanishka-mittal@users.noreply.github.com>
2022-04-07 19:04:08 +0530

    Update client-phase3.cpp

--- client-phase3.cpp
+++ client-phase3.cpp
@@ -1,26 +1,109 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
+#include <string>
 #include <cstdio>
 #include <iostream>
 #include <fstream>
 #include <arpa/inet.h>
 #include <dirent.h>
 #include <bits/stdc++.h>
+// #include <thread>
 int INTMAX=1000000007;
-
-//yet to make hash unhash functions
-
-string calc_MD5(string content){
-
-}
-
-string unhash(string hash){
-
-}
-
 using namespace std;
+typedef unsigned char BYTE;
+
+static const std::string base64_chars =
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "0123456789+/";
+
+
+static inline bool is_base64(BYTE c) {
+  return (isalnum(c) || (c == '+') || (c == '/'));
+}
+
+std::string base64_encode(BYTE const* buf, unsigned int bufLen) {
+  std::string ret;
+  int i = 0;
+  int j = 0;
+  BYTE char_array_3[3];
+  BYTE char_array_4[4];
+
+  while (bufLen--) {
+    char_array_3[i++] = *(buf++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for(i = 0; (i <4) ; i++)
+        ret += base64_chars[char_array_4[i]];
+      i = 0;
+    }
+  }
+
+  if (i)
+  {
+    for(j = i; j < 3; j++)
+      char_array_3[j] = '\0';
+
+    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+    char_array_4[3] = char_array_3[2] & 0x3f;
+
+    for (j = 0; (j < i + 1); j++)
+      ret += base64_chars[char_array_4[j]];
+
+    while((i++ < 3))
+      ret += '=';
+  }
+
+  return ret;
+}
+
+std::vector<BYTE> base64_decode(std::string const& encoded_string) {
+  int in_len = encoded_string.size();
+  int i = 0;
+  int j = 0;
+  int in_ = 0;
+  BYTE char_array_4[4], char_array_3[3];
+  std::vector<BYTE> ret;
+
+  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+    char_array_4[i++] = encoded_string[in_]; in_++;
+    if (i ==4) {
+      for (i = 0; i <4; i++)
+        char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+      for (i = 0; (i < 3); i++)
+          ret.push_back(char_array_3[i]);
+      i = 0;
+    }
+  }
+
+  if (i) {
+    for (j = i; j <4; j++)
+      char_array_4[j] = 0;
+
+    for (j = 0; j <4; j++)
+      char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+    for (j = 0; (j < i - 1); j++) ret.push_back(char_array_3[j]);
+  }
+
+  return ret;
+}
 
 int main(int argc, char *argv[]){
     int sno,id,myport,nbrs;
@@ -32,13 +115,11 @@ int main(int argc, char *argv[]){
         cin>>nos[i]>>ports[i];
     }
     int nfiles; cin>>nfiles;
-    vi files(nfiles);
+    vector<string> files(nfiles);
     for(int i=0;i<nfiles;i++){
         cin>>files[i];
     }
-
     set <string> myfiles;
-    map <string,string> filehash;
     set <string> connects;
     DIR *pDIR;
     struct dirent *entry;
@@ -46,7 +127,6 @@ int main(int argc, char *argv[]){
         while(entry = readdir(pDIR)){
             if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
                 myfiles.insert(entry->d_name);
-                filehash[entry->d_name]=calc_MD5(entry->d_name);
             }
         closedir(pDIR);
     }
@@ -72,18 +152,22 @@ int main(int argc, char *argv[]){
         exit(0); 
     }
     int sendfd[nbrs];
+    vector<int> recfds;
     fd_set master,read_fds;
     int fdmax,nbytes,n_bytes;
     //change buffer size
-    char buf[256];
+    char buf[1024];
     FD_ZERO(&master);
     FD_ZERO(&read_fds);
     FD_SET(recfd, &master);
     fdmax = recfd;
     map <int,int> nos_map;
     map <int, int> port_map;
+    map <int, int> i_map;
     map <string, int> filemap;
-    for(auto f: myfiles){
+    map <string, int> pendingmap;
+    map <int,int> IDs;
+    for(auto f: files){
         filemap[f]=INTMAX;
     }
     for(int i=0;i<nbrs;i++){
@@ -97,9 +181,11 @@ int main(int argc, char *argv[]){
         FD_SET(sendfd[i], &master);
         port_map[sendfd[i]]=ports[i];
         nos_map[sendfd[i]]=nos[i];
+        i_map[sendfd[i]]=i;
         fdmax=max(fdmax,sendfd[i]);
     }
-    int count=0;
+    int count=nbrs;
+    bool tick=0;
     for(;;){
         read_fds=master;
         if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1){
@@ -114,25 +200,25 @@ int main(int argc, char *argv[]){
                     if(neighfd==-1){
                         perror("accept");
                     }else{
-                        nbytes=send(neighfd, to_string(id).c_str(), strlen(to_string(id).c_str()), 0);
+                        FD_SET(neighfd, &master);
+                        fdmax=max(fdmax,neighfd);
+                        recfds.push_back(neighfd);
+                        nbytes=send(neighfd, ("&"+to_string(id)).c_str(), strlen(("&"+to_string(id)).c_str()), 0);
                         if(nbytes<= 0){
                             if (nbytes == 0) {
-                                printf("selectserver: socket %d hung up\n", i);
+                                // printf("selectserver: socket %d hung up\n", i);
+                                FD_CLR(i,&master);
                                 shutdown(neighfd,2);
                             } else {
                                 perror("send");
                             }
                         }
-                        for(auto f: files){
-                            string filename='$'+f;
-                            n_bytes=send(neighfd, to_string(filename).c_str(), strlen(to_string(filename).c_str()), 0);
-                        }
                     }
                 }else{
-                    // cout<<"here\n";
                     nbytes=recv(i, buf, sizeof(buf), 0);
                     if (nbytes == 0) {
-                        printf("selectserver: socket %d hung up\n", i);
+                        // printf("selectserver: socket %d hung up\n", i);
+                        FD_CLR(i,&master);
                         shutdown(i,2);
                     }
                     else if(nbytes<0){
@@ -140,46 +226,221 @@ int main(int argc, char *argv[]){
                     }
                     else{ 
                         string s=buf;
-                        if(s[0]=='$'){
+                        memset( buf, '\0', sizeof(char)*1024 );
+                        if(s[0]=='&'){
+                            // cout<<s<<endl;
                             s.erase(0,1);
-                            if(myfiles.count(s)==1){
-                                string ye='#'+s;
-                                n_bytes=send(i, to_string(ye).c_str(), strlen(to_string(ye).c_str()), 0);
-                            }
-                        }
-
-                        if(s[0]=='#'){
-                            s.erase(0,1);
-                            string name="";
-                            int start=0;
-                            for(int i=0;i<s.len();i++){
-                                if(s[i]=='%'){
-                                    start=i+1;
-                                }
-                                name.push_back(s[i])
-                            }
-                            filemap[name]=min(filemap[name],i);
-                            string content="";
-                            for(int i=start;i<s.len();i++){
-                                content.push_back(s[i]);
-                            }
-                            ofstream My_file(name);
-                            My_file << unhash(content);
-                            My_file.close();
-                        }
-
-                        else{
-                            cout<<"Connected to " <<nos_map[i]<< " with unique-ID "<<s<<" on port "<<port_map[i]<<"\n";
+                            int x=stoi(s);
+                            IDs[i]=x;
+                            cout<<"Connected to " <<nos_map[i]<< " with unique-ID "<<x<<" on port "<<port_map[i]<<"\n";
                             connects.insert(s);
-                            if(connects.size()>=nbrs) break;
+                            if(connects.size()>=nbrs){
+                                break;
+                            }
+                        }else{
+                            s.erase(0,1);
+                            pendingmap[s]=i;
                         }
                     }
                 }
             }
         }
+        if(connects.size()>=nbrs) break;
     }
-    for (auto *it=filemap.begin(); it!=filemap.end();it++){
+    // cout<<"here\n";
+    memset( buf, '\0', sizeof(char)*1024 );
+    int crntfile=0;
+    count=nbrs;
+    for(int i=0;i<nbrs;i++){
+        string filename='$'+files[0];
+        n_bytes=send(sendfd[i], filename.c_str(), strlen(filename.c_str()), 0);
+        // cout<<n_bytes<<endl;
+    }
+    int confirm=0;
+    for (auto it=pendingmap.begin(); it!=pendingmap.end();it++){
+        string s=it->first;
+        int soc=it->second;
+        if(myfiles.count(s)==1){
+            string ye='#'+s;
+            n_bytes=send(soc, ye.c_str(), strlen(ye.c_str()), 0);
+            cout<<n_bytes<<endl;
+        }else{
+            string ye="%"+s;
+            n_bytes=send(soc, ye.c_str(), strlen(ye.c_str()), 0);
+        }
+    }
+    FD_CLR(recfd,&master);
+    for(;;){
+        read_fds=master;
+        if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1){
+            perror("select");
+            exit(1);
+        }
+        for(int i=0;i<=fdmax;i++){
+            if (FD_ISSET(i,&read_fds)){
+                // cout<<i<<" ";
+                nbytes=recv(i, buf, sizeof(buf), 0);
+                // cout<<buf<<endl;
+                if (nbytes == 0) {
+                    // printf("selectserver: socket %d hung up\n", i);
+                    FD_CLR(i,&master);
+                    shutdown(i,2);
+                }
+                else if(nbytes<0){
+                    perror("recv");
+                }
+                else{ 
+                    string s=buf;
+                    // cout<<s<<endl;
+                    memset( buf, '\0', sizeof(char)*1024 );
+                    if(s[0]=='$'){
+                        // cout<<s<<endl;
+                        s.erase(0,1);
+                        if(myfiles.count(s)==1){
+                            string ye='#'+s;
+                            n_bytes=send(i, ye.c_str(), strlen(ye.c_str()), 0);
+
+                        }else{
+                            string ye="%"+s;
+                            n_bytes=send(i, ye.c_str(), strlen(ye.c_str()), 0);
+                        }
+                    }else if(s[0]=='#'){
+                        // cout<<"Found "<<s<<" "<<IDs[i]<<endl;
+                        s.erase(0,1);
+                        filemap[s]=min(filemap[s],i);
+                        count--;
+                        if(count==0){
+                            break;
+                        }
+                    }else if(s[0]=='%'){
+                        s.erase(0,1);
+                        count--;
+                        if(count==0){
+                            break;
+                        }
+                    }else if(s[0]=='&'){
+                        // cout<<s<<endl;
+                        confirm++;
+                        // cout<<crntfile<<" "<<count<<endl;
+                        // count--;
+                    }
+                }
+            }
+        }
+        if(count==0 || crntfile>=nfiles){
+            tick=0;
+            count=nbrs;
+            crntfile++;
+            // cout<<confirm<<" "<<crntfile<<endl;
+            if(confirm==nbrs && crntfile>nfiles){
+                count=0;
+                break;
+            }else if(crntfile==nfiles){
+                count=0;
+                // cout<<"here\n";
+                for(int i=0;i<nbrs;i++){
+                    string star="&"+to_string(id);
+                    n_bytes=send(sendfd[i], star.c_str(), strlen(star.c_str()), 0);
+                }
+                if(confirm==nbrs){
+                    break;
+                }
+            }else if(crntfile<nfiles){
+                for(int i=0;i<nbrs;i++){
+                    string filename='$'+files[crntfile];
+                    // cout<<filename<<endl;
+                    n_bytes=send(sendfd[i], filename.c_str(), strlen(filename.c_str()), 0);
+                    // cout<<nbytes<<endl;
+                }
+            }
+        }
+    }
+    for (auto it=filemap.begin(); it!=filemap.end();it++){
         if(it->second==INTMAX) cout<<"Found "<<it->first<<" at 0 with MD5 0 at depth 0"<<endl;
-        else cout<<"Found "<<it->first<<" at "<<it->second<<" with MD5 "<<filehash[s]<<" at depth 1"<<endl;
+        else cout<<"Found "<<it->first<<" at "<<IDs[it->second]<<" with MD5 0 at depth 1"<<endl;
+    }
+    map<int,set<string>> port_files;
+    int foundfiles=0;
+    for (auto it=filemap.begin(); it!=filemap.end();it++){
+        if(it->second!=INTMAX){
+            foundfiles++;
+            port_files[it->second].insert(it->first);
+            cout<<it->second<<" "<<it->first<<endl;
+        }
+
+    }
+    
+    // string filecontent="";
+    for (auto it=port_files.begin(); it!=port_files.end();it++){
+        string s=*((it->second).begin());
+        int soc=it->first;
+        string filename='$'+s;
+        n_bytes=send(soc, filename.c_str(), strlen(filename.c_str()), 0);
+        // cout<<n_bytes<<endl;
+    }
+    // int leftfiles=foundfiles;
+    for(;;){
+        read_fds=master;
+        if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1){
+            perror("select");
+            exit(1);
+        }
+        for(int i=0;i<=fdmax;i++){
+            if (FD_ISSET(i,&read_fds)){
+                nbytes=recv(i, buf, sizeof(buf), 0);
+                if (nbytes == 0) {
+                    // printf("selectserver: socket %d hung up\n", i);
+                    FD_CLR(i,&master);
+                    shutdown(i,2);
+                }
+                else if(nbytes<0){
+                    perror("recv");
+                }
+                else{ 
+                    string s=buf;
+                    memset( buf, '\0', sizeof(char)*1024 );
+                    if(s[0]=='$'){
+                        s.erase(0,1);
+                        FILE *picture;
+                        picture = fopen(("sample-data/files/client"+to_string(sno)+"/"+s).c_str(), "r");
+                        fseek(picture, 0, SEEK_SET);
+                        printf("Sending Picture as Byte Array\n");
+                        unsigned char send_buffer[1024];
+                        while(!feof(picture)){
+                            fread(send_buffer, 1, sizeof(send_buffer), picture);
+                            n_bytes=send(i, send_buffer,1024, 0);
+                            memset( send_buffer, '\0', sizeof(char)*1024 );
+                        }
+                        fclose(picture);
+                    }else{
+                        string name =*((port_files[i]).begin());
+                        //FILE *picture = fopen(name.c_str(), "w");
+                        //fprintf(picture,s.c_str());
+                        cout<<s;
+                        // cout<<nbytes<<endl;
+                        while(recv(i, buf, sizeof(buf), 0)>0){
+                            //cout<<nbytes<<endl;
+                            //fprintf(picture,buf);
+                            cout<<buf;
+                            memset( buf, '\0', sizeof(buf));
+                        }
+                        //fclose(picture);
+                        port_files[i].erase(name);
+                        if(!port_files[i].empty()){
+                            name=*((port_files[i]).begin());
+                            string filename='$'+name;
+                            n_bytes=send(i, filename.c_str(), strlen(filename.c_str()), 0);
+                        }
+                        // foundfiles--;
+                        // if(foundfiles==0){
+                        //     break;
+                        // }
+                    }
+                }
+            }
+        }
+        // if(foundfiles==0){
+        //     break;
+        // }
     }
 }
